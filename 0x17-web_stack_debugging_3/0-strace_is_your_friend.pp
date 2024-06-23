5c51dcad9a579f59e759581419e02bcf48343433
Medmat0 <yassineelmatrour@gmail.com>
2024-06-23 19:21:10 +0000

    fix task 1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,8 @@
 # Puppet manifest to fix Apache 500 error issue
 # Fixing permissions on Wordpress directory
+# Fix 500 error when a GET HTTP method is requested to Apache web server
 
-file { '/var/www/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  recurse => true,
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/var/www/html'],
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
